306e48256a9fcda2b0aa8f84ae7796303d688ff9
CaptainMorghan <47458916+CaptainMorghan@users.noreply.github.com>
2019-05-30 11:23:36 -0400

    Add files via upload

---
+++ Dice.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data; 
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Yahtzee
+{
+    class Dice
+    {
+        //Variables -  Canvas, Rectangle, Ellipse[], int
+        Canvas canvas;
+        Ellipse[] dots;
+        Rectangle diceRectangle;
+        int drawSize ;
+        public int dieValue;
+        /// <summary>
+        /// Creates a dice
+        /// </summary>
+        /// <param name="c">The canvas to draw on</param>
+        /// <param name="p">A starting point on the canvas for the top-left corner</param>
+        public Dice(Canvas c, Point p)
+        {
+            canvas = c;
+            //set up the rectanlge
+
+            diceRectangle = new Rectangle();
+            diceRectangle.Fill = Brushes.White;
+            diceRectangle.Stroke = Brushes.Black;
+            drawSize = 100;
+            diceRectangle.Height = drawSize;
+            diceRectangle.Width = drawSize;
+            canvas.Children.Add(diceRectangle);
+            Canvas.SetLeft(diceRectangle, p.X);
+            Canvas.SetTop(diceRectangle, p.Y);
+
+            dots = new Ellipse[9];
+            for (int i = 0; i < dots.Length; i++)
+            {
+                dots[i] = new Ellipse();
+                dots[i].Fill = Brushes.Black;
+                dots[i].Height = drawSize / 9;
+                dots[i].Width = dots[i].Height;
+                canvas.Children.Add(dots[i]);
+                Canvas.SetLeft(dots[i], p.X + (dots[i].Width*2 * ((i%3)+1)));
+                Canvas.SetTop(dots[i], p.Y + ((i / 3)+1) * dots[i].Height*2);
+            }
+        }
+        public void drawDie()
+        {
+            clearDie();
+            if (dieValue == 1)
+            {
+                dots[4].Fill = Brushes.Black;
+            }
+            else if (dieValue == 2)
+            {
+                dots[0].Fill = Brushes.Black;
+                dots[8].Fill = Brushes.Black;
+            }
+            else if (dieValue == 3)
+            {
+                dots[0].Fill = Brushes.Black;
+                dots[4].Fill = Brushes.Black;
+                dots[8].Fill = Brushes.Black;
+            }
+            else if (dieValue == 4)
+            {
+                dots[0].Fill = Brushes.Black;
+                dots[2].Fill = Brushes.Black;
+                dots[6].Fill = Brushes.Black;
+                dots[8].Fill = Brushes.Black;
+            }
+            else if (dieValue == 5)
+            {
+                dots[0].Fill = Brushes.Black;
+                dots[2].Fill = Brushes.Black;
+                dots[4].Fill = Brushes.Black;
+                dots[6].Fill = Brushes.Black;
+                dots[8].Fill = Brushes.Black;
+            }
+            else if (dieValue == 6)
+            {
+                dots[0].Fill = Brushes.Black;
+                dots[2].Fill = Brushes.Black;
+                dots[3].Fill = Brushes.Black;
+                dots[5].Fill = Brushes.Black;
+                dots[6].Fill = Brushes.Black;
+                dots[8].Fill = Brushes.Black;
+            }
+           
+            
+        }
+            public void clearDie()
+        {
+            for (int i = 0; i < dots.Length; i++)
+            {
+                dots[i].Fill = Brushes.White;
+            }
+        }
+    }
+
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Yahtzee
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //variables for dice
+        int Dice;
+        int Dice2;
+        int Dice3;
+        int Dice4;
+        int Dice5;
+        //variables for what box you select for scoring
+        //UpperSection
+        int Aces;
+        int Twos;
+        int Threes;
+        int Fours;
+        int Fives;
+        int Sixes;
+        int UpperSection;
+        int UpperSectionSubTotal;
+        int UpperSectionBonus;
+        int UpperSectionTotal;
+        //LowerSection
+        int ThreeOfAKind;
+        int FourOfAKind;
+        int FullHouse;
+        int SmallStraight;
+        int LargeStraight;
+        int Yahtzee;
+        int YahtzeeBonus;
+        int YahtzeeBonus2;
+        int BottomSectionTotal;
+        int GrandTotal;
+        //Variables for the amount of a certin number this willbe needed to calculate score for allupper section points and certin lower section points
+        int NumberOfAces;
+        int NumberOfTwos;
+        int NumberOfThrees;
+        int NumberOfFours;
+        int NumberOfFives;
+        int NumberOfSixes;
+
+        Dice dice;
+        Dice diceTwo;
+        Dice diceThree;
+        Dice diceFour;
+        Dice diceFive;
+       
+
+
+
+        Random random = new Random();
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //reposition later
+            dice = new Dice(canvas, new Point(10, 10));
+            diceTwo = new Dice(canvas, new Point(120, 10));
+            diceThree = new Dice(canvas, new Point(230, 10));
+            diceFour = new Dice(canvas, new Point(340, 10));
+            diceFive = new Dice(canvas, new Point(450, 10));
+            
+        }
+
+        private void RollDice_Click(object sender, RoutedEventArgs e)
+        {
+            Dice = random.Next(1, 7);
+            dice.dieValue = Dice;
+            dice.drawDie();
+
+            Dice2 = random.Next(1, 7);
+            diceTwo.dieValue = Dice2;
+            diceTwo.drawDie();
+
+            Dice3 = random.Next(1, 7);
+            diceThree.dieValue = Dice3;
+            diceThree.drawDie();
+
+            Dice4 = random.Next(1, 7);
+            diceFour.dieValue = Dice4;
+            diceFour.drawDie();
+
+            Dice5 = random.Next(1, 7);
+            diceFive.dieValue = Dice5;
+            diceFive.drawDie();
+            
+            
+        }
+
+        private void Ones_Click(object sender, RoutedEventArgs e)
+        {
+            lblOnes.Content = ("Dice 1: " + dice.dieValue.ToString() + "\n" + "Dice 2: " + diceTwo.dieValue.ToString());
+            
+               
+            }
+            
+
+        }
+    }
+
